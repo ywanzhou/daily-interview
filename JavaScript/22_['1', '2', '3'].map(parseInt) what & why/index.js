c2e19a93d76142ac15b169029511b2a41433ae01
Zhou-Zhonghua <seecode@qq.com>
2021-12-27 21:28:50 +0800

    map函数基础使用

--- JavaScript/22_['1', '2', '3'].map(parseInt) what & why/index.js
+++ JavaScript/22_['1', '2', '3'].map(parseInt) what & why/index.js
@@ -1,3 +1,13 @@
-console.log(parseInt(10, 0))
-console.log(parseInt(10, 1)) // NaN
-console.log(parseInt(10, 37)) // NaN
+function doubleNum(n, i) {
+  let doubleN = 2 * n
+  console.log(`第{i+1}个元素的的二倍是${doubleN}`)
+  return doubleN
+}
+let newArr = [1, 2, 3].map(doubleNum)
+console.log(newArr)
+/* 执行结果如下：
+第{i+1}个元素的的二倍是2
+第{i+1}个元素的的二倍是4
+第{i+1}个元素的的二倍是6
+[ 2, 4, 6 ]
+ */
