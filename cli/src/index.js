9ebac8912f6b0b41f94593c7cbe93134ea2d050a
ywanzhou <ywanzhou@foxmail.com>
2022-06-02 21:39:02 +0800

    入口文件

--- cli/src/index.js
+++ cli/src/index.js
@@ -1,3 +1,23 @@
 const content = require('./getContent')
 
-console.log(content)
+const fs = require('fs')
+const path = require('path')
+
+function readFile(name) {
+  return fs.readFileSync(path.join(__dirname, '..', 'template', name), {
+    encoding: 'utf8',
+    flag: 'r',
+  })
+}
+
+const header = readFile('header.txt')
+const footer = readFile('footer.txt')
+
+const readme = header + content + footer
+
+try {
+  fs.writeFileSync(path.join(__dirname, '..', '..', 'README.md'), readme)
+  console.log('更新成功')
+} catch (error) {
+  console.log(error)
+}
